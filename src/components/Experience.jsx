51016008dc9df9296adeac42858464c532201ed7
Canberk Varli <canberkvarli@gmail.com>
2024-01-04 22:23:56 -0800

    update my resume button

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,9 +7,9 @@ import 'react-vertical-timeline-component/style.min.css';
 import { styles } from '../styles';
 import { experiences } from '../constants';
 import { SectionWrapper } from '../hoc';
-import { download, downloadHover, resume } from '../assets';
+import { canberk, download, downloadHover, resume } from '../assets';
 import { textVariant } from '../utils/motion';
-
+import canberk_resume from "../assets/personal/Resume.pdf"
 const ExperienceCard = ({ experience }) => (
   <VerticalTimelineElement
     contentStyle={{
@@ -91,39 +91,44 @@ const Experience = () => {
                 />
               </div>
             }>
-            <button
-              className="live-demo flex justify-between 
-              sm:text-[18px] text-[14px] text-timberWolf 
-              font-bold font-beckman items-center py-5 pl-3 pr-3 
-              whitespace-nowrap gap-1 sm:w-[148px] sm:h-[58px] 
-              w-[125px] h-[46px] rounded-[10px] bg-jetLight 
-              sm:mt-[22px] mt-[16px] hover:bg-battleGray 
-              hover:text-eerieBlack transition duration-[0.2s] 
-              ease-in-out"
-              onClick={() =>
-                window.open(
-                  '/canberkvarli.github.io/src/assets/personal/Resume.pdf',
-                  '_blank'
-                )
-              }
-              onMouseOver={() => {
-                document
-                  .querySelector('.download-btn')
-                  .setAttribute('src', downloadHover);
-              }}
-              onMouseOut={() => {
-                document
-                  .querySelector('.download-btn')
-                  .setAttribute('src', download);
-              }}>
-              MY RESUME
-              <img
-                src={download}
-                alt="download"
-                className="download-btn sm:w-[26px] sm:h-[26px] 
-                w-[23px] h-[23px] object-contain"
-              />
-            </button>
+            <a href={canberk_resume}
+              target="_blank"
+              download="Canberk-Resume"
+              >
+              <button
+                className="live-demo flex justify-between 
+                sm:text-[18px] text-[14px] text-timberWolf 
+                font-bold font-beckman items-center py-5 pl-3 pr-3 
+                whitespace-nowrap gap-1 sm:w-[148px] sm:h-[58px] 
+                w-[125px] h-[46px] rounded-[10px] bg-jetLight 
+                sm:mt-[22px] mt-[16px] hover:bg-battleGray 
+                hover:text-eerieBlack transition duration-[0.2s] 
+                ease-in-out"
+                // onClick={() =>
+                //   window.open(
+                //     'src/assets/personal/Resume.pdf',
+                //     '_blank'
+                //   )
+                // }
+                onMouseOver={() => {
+                  document
+                    .querySelector('.download-btn')
+                    .setAttribute('src', downloadHover);
+                }}
+                onMouseOut={() => {
+                  document
+                    .querySelector('.download-btn')
+                    .setAttribute('src', download);
+                }}>
+                MY RESUME
+                <img
+                  src={download}
+                  alt="download"
+                  className="download-btn sm:w-[26px] sm:h-[26px] 
+                  w-[23px] h-[23px] object-contain"
+                />
+              </button>
+            </a>
           </VerticalTimelineElement>
         </VerticalTimeline>
       </div>
